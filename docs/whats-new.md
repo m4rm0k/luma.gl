7f3b3c5677c4d2b9f37ec30f3c279dd0a8a5dc6a
Georgios Karnas <georgios@uber.com>
2019-03-01 11:19:49 -0800

    WebVR VRAnimationLoop

--- docs/whats-new.md
+++ docs/whats-new.md
@@ -2,6 +2,11 @@
 
 ## Version 7.x
 
+### WebVR Support (experimental)
+
+Just replace your `AnimationLoop` with `VRAnimationLoop` from `@luma.gl/addons`.
+Works with [Firefox Reality](https://mixedreality.mozilla.org/firefox-reality/).
+
 ### "Asynchronous" Textures
 
 The `Texture` class now supports image data being initialized with a URL `string` or a `Promise`that resolves to any of the previously valid data types, e.g. an `Image` instance. This avoids the need to clutter your code with `promise.then()` and/or callback functions just to load textures.
--- examples/gltf/app.js
+++ examples/gltf/app.js
@@ -1,8 +1,9 @@
+/* global document */
 import {GLTFParser} from '@loaders.gl/gltf';
 import {DracoDecoder} from '@loaders.gl/draco';
-import {AnimationLoop, setParameters, clear, createGLTFObjects, log} from 'luma.gl';
+import {setParameters, clear, createGLTFObjects, log} from '@luma.gl/core';
+import {VRAnimationLoop} from '@luma.gl/addons';
 import {Matrix4, radians} from 'math.gl';
-import document from 'global/document';
 
 export const GLTF_BASE_URL =
   'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/';
@@ -11,6 +12,7 @@ const GLTF_MODEL_INDEX = `${GLTF_BASE_URL}model-index.json`;
 const INFO_HTML = `
 <p><b>glTF</b> rendering.</p>
 <p>A luma.gl <code>glTF</code> renderer.</p>
+<p><img src="https://img.shields.io/badge/WebVR-Supported-orange.svg" /></p>
 <div>
   Model
   <select id="modelSelector">
@@ -268,7 +270,7 @@ export class DemoApp {
     };
   }
 
-  onInitialize({gl, canvas}) {
+  onInitialize({gl, canvas, _loop: animationLoop}) {
     setParameters(gl, {
       depthTest: true,
       blend: false
@@ -371,8 +373,7 @@ export class DemoApp {
     });
   }
 
-  onRender({gl, time, width, height, aspect}) {
-    gl.viewport(0, 0, width, height);
+  onRender({gl, time, aspect, vrViewMatrix, vrProjectionMatrix}) {
     clear(gl, {color: [0.2, 0.2, 0.2, 1.0], depth: true});
 
     const [pitch, roll] = this.rotation;
@@ -382,12 +383,15 @@ export class DemoApp {
       this.translate * Math.cos(roll) * Math.cos(-pitch)
     ];
 
-    const uView = new Matrix4()
+    // TODO: find how to avoid using Array.from() to convert TypedArray to regular array
+    const uView = new Matrix4(vrViewMatrix ? Array.from(vrViewMatrix) : null)
       .translate([0, 0, -this.translate])
       .rotateX(pitch)
       .rotateY(roll);
 
-    const uProjection = new Matrix4().perspective({fov: radians(40), aspect, near: 0.1, far: 9000});
+    const uProjection = vrProjectionMatrix
+      ? new Matrix4(Array.from(vrProjectionMatrix))
+      : new Matrix4().perspective({fov: radians(40), aspect, near: 0.1, far: 9000});
 
     if (!this.scenes.length) return false;
 
@@ -421,7 +425,7 @@ export class DemoApp {
   }
 }
 
-const animationLoop = new AnimationLoop(new DemoApp());
+const animationLoop = new VRAnimationLoop(new DemoApp());
 
 animationLoop.getInfo = () => INFO_HTML;
 
